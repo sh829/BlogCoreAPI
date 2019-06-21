da8965c172c3c954cee89a9e1469f79d49e9f48e
xiaomi_global <243237408@qq.com>
2019-06-21 17:47:44 +0800

    添加项目文件。

---
+++ BlogCore.Model/Love.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BlogCore.Model
+{
+    public class Love
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
---
+++ BlogCore/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BlogCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Version = "v0.1.0",
+                    Title = "blog.core.api",
+                    Description="框架说明文档",
+                    TermsOfService="None",
+                    Contact=new Swashbuckle.AspNetCore.Swagger.Contact {Name="Blog.Core",
+                        Email ="243237408@qq.com",Url= "https://www.jianshu.com/u/94102b59cc2a"
+                    }
+                });
+                var basePath = Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "BlogCore.xml");//这个就是刚刚配置的xml文件名
+                c.IncludeXmlComments(xmlPath, true);//默认的第二个参数是false，这个是controller的注释，记得修改
+                var xmlModelPath = Path.Combine(basePath, "BlogCoreModel.xml");
+                c.IncludeXmlComments(xmlModelPath);
+            });
+
+            #endregion
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                #region swagger
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHple V1");
+                    c.RoutePrefix = "";
+                });
+                #endregion
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+           
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
